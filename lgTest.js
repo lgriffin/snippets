61602bd4558c68c8bb1c2160e92f42d9ce4a831b
Leigh Griffin <leighgriffin@gmail.com>
2015-10-28 15:08:24 +0000

    added test for Q2
    Q2 test

---
+++ lgTest.js
@@ -0,0 +1,12 @@
+var Q2 = require('./Q2.js');
+
+var callOneValue;
+var callOne = Q2.callTwoService(function(err,num){
+
+	console.log(num);
+
+});
+	
+	
+
+
---
+++ test/Q2.test.js
@@ -0,0 +1,46 @@
+// Simple test for Q2 using mocha to run the test and using a BDD assertion approach
+var assert = require('assert');
+var Q2 = require('../Q2.js');
+
+// Testing the three main functions
+describe('Maths test', function() {
+    it('should return a 1 from CallOneService');
+    it('should return a 2 from CallTwoService');
+    it('should return a 3 from remoteMathService');
+});
+
+// This should expect 1 to be returned from the function
+it('should return a 1 from CallOneService', function(done) {
+
+    Q2.callOneService(function(err, num) {
+
+        assert.equal(num, 1);
+        done();
+    });
+
+
+});
+
+// This should expect 2 to be returned from the function
+it('should return a 2 from CallTwoService', function(done) {
+
+    Q2.callTwoService(function(err, num) {
+
+        assert.equal(num, 2);
+        done();
+    });
+
+
+});
+
+// This should expect 3 to be returned from the function
+it('should return a 3 from remoteMathService', function(done) {
+
+    Q2.remoteMathService(function(err, answer) {
+
+        assert.equal(answer, 3);
+        done();
+    });
+
+
+});
